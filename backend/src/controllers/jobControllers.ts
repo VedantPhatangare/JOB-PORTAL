e87b9b469a80db0fba26991b91122d9e5030b185
Vedant <vedantphatangare10@gmail.com>
2025-02-14 11:43:43 +0530

    created by  handled

--- backend/src/controllers/jobControllers.ts
+++ backend/src/controllers/jobControllers.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import asyncHandler from "../utils/asyncHandler.js";
 import Job from "../models/job.model.js";
+import { UserPayload } from "../types/userPayload.js";
 
 export const createJob = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
-        const {title,description,company,location,salary,jobtype,postedBy} = req.body
-        const job = new Job({title,description,company,location,salary,jobtype,postedBy})
+        const {title,description,company,location,salary,jobtype} = req.body
+        const {id}= req.user as UserPayload;
+        const job = new Job({title,description,company,location,salary,jobtype,postedBy:id})
         await job.save();
         return res.status(201).json({message:"Job created successfully", job});
 })
