87f037fe84d10d4033958c957c4df2b78d08dc37
Vedant <vedantphatangare10@gmail.com>
2025-02-22 15:52:32 +0530

    Job controller fixed bug of id instead of nameof job poster

--- backend/src/controllers/jobControllers.ts
+++ backend/src/controllers/jobControllers.ts
@@ -1,17 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import asyncHandler from "../utils/asyncHandler.js";
 import Job from "../models/job.model.js";
+import User from "../models/user.model.js";
 
 export const createJob = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
         const {title,description,company,location,salary,jobtype} = req.body
         const id= req.user?.id
-        const job = new Job({title,description,company,location,salary,jobtype,postedBy:id})
-        await job.save();
-        return res.status(201).json({message:"Job created successfully", job});
+        const user = await User.findOne({_id:id})
+        if(user){
+                const {name,_id:id} = user
+                const job = new Job({title,description,company,location,salary,jobtype,postedBy:{name,id}})
+                await job.save();
+                return res.status(201).json({message:"Job created successfully", job});
+        }
 })
 
 export const getJobs = asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
         const jobs = await Job.find();
         res.status(201).json({message:"Jobs fetched successfully",jobs});
         return;
-})
+});
