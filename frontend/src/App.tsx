d70cf0ee0216e2cbab5adde95502164aa96b2979
Vedant <vedantphatangare10@gmail.com>
2025-02-22 15:49:42 +0530

    Frontend Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 function App() {
 
   return (
-    <>
-    <div className='bg-red-700'>
-     
+    <div className='relative flex flex-col items-center w-full h-[100vh] bg-[#f8f8f8]'>
+      <Navbar/>
+      <Outlet/>
     </div>
-    </>
+  
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+import { Provider } from 'react-redux'
+import store from './app/Store'
+
+const Router  = createBrowserRouter([
+    {
+      path:"/",
+      element:<App/>,
+      children:[
+        {
+          path:'/',
+          element:<Home/>
+        }
+        ,
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/signup",
+          element:<Signup/>
+        }
+      ]
+    }
+])
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store = {store}>
+    <RouterProvider router={Router}/>
+  </Provider>
 )
