fcd2447cdb36d4a8d780965ff8c4f950c7e1b7c3
Vedant <vedantphatangare10@gmail.com>
2025-02-23 00:27:29 +0530

    added new JobReducer to store Jobs as State

--- frontend/src/app/Store.ts
+++ frontend/src/app/Store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import Authreducer from "../features/Authslice";
-
+import JobReducer from '../features/Jobslice'
 const store = configureStore({
     reducer: {
-        auth: Authreducer
+        auth: Authreducer,
+        jobs: JobReducer
     }
 });
 
 export type RootState = ReturnType<typeof store.getState>
+export type Appdispatch = typeof store.dispatch
 
 export default store;
