8dcc4d00840df1e6e94493be4d9aacf455104b50
Vedant <vedantphatangare10@gmail.com>
2025-03-08 14:07:53 +0530

    changed error message

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -3,14 +3,14 @@ import jwt  from "jsonwebtoken";
 import { UserPayload } from "../types/reqTypes.js";
 const jwtVerify = (req:Request, res:Response, next:NextFunction):void=>{
     const token = req.headers.authorization?.split(" ")[1];
-    if(!token){
-        res.status(401).json({message:"Unauthorized access, token missing"});
-        return;
-    }
     try {
+      if(!token){
+          res.status(401).json({message:"Unauthorized access,Please Login First"});
+          return;
+      }
       const decoded = jwt.verify(token, process.env.JWT_SECRET!);
       req.user = decoded as UserPayload;
-    //   console.log(req.user);
+      //   console.log(req.user);
       next();
     } catch (error) {
         res.status(401).json({message:"Unauthorized access, invalid token"});
