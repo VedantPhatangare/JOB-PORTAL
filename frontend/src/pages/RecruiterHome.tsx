2420b32944159fe410ac9b8e6b88ccb1da8d7a32
Vedant <vedantphatangare10@gmail.com>
2025-03-09 10:17:38 +0530

    started recruiter home page

--- frontend/src/pages/RecruiterHome.tsx
+++ frontend/src/pages/RecruiterHome.tsx
@@ -1,19 +1,32 @@
-import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../app/Store";
+import { useSearchParams } from "react-router-dom";
+import { JobcardProps } from "../utils/types";
 
 const RecruiterHome = () => {
-  const getPostedjobs= async()=>{
-      // await axios.
-  }
+
+  const [searchParams] = useSearchParams()
+  const [newJobs, setnewJobs] = useState<JobcardProps[]>([])
+  const jobs = useSelector((state: RootState) => state.jobs.jobs);
+  const id= searchParams.get('id') as string
+  const getPostedjobs = () => {
+    let newjobs=jobs.filter((job) => job.postedBy.id == id);
+    setnewJobs(newjobs);
+  };
   useEffect(() => {
     getPostedjobs();
-  }, [])
-  
-  return (
-    <div>
-        home
-    </div>
-  )
-}
+  }, [jobs,id]);
 
-export default RecruiterHome
+  return <div>
+    {
+      (newJobs.length != 0 && newJobs)?
+        newJobs.map((job)=>
+          <div>{job._id}</div>
+        )
+      : "No Job Post from You"
+    }
+    </div>;
+};
+
+export default RecruiterHome;
