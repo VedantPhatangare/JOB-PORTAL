c787ba68b96ac8e68594f88e99f0b048c02999d9
Vedant <vedantphatangare10@gmail.com>
2025-03-15 00:11:21 +0530

    ts type

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -9,3 +9,17 @@ export interface JobcardProps {
     postedBy: {name:string, id:string};
     createdAt?:Date
   }
+
+  export interface jobPostInput{
+    value: string | null | undefined,
+    message: string
+  }
+
+  export interface jobPostForm{
+    title: jobPostInput,
+    company: jobPostInput,
+    location: jobPostInput,
+    salary: jobPostInput,
+    jobtype:jobPostInput ,
+    description: jobPostInput
+  }
