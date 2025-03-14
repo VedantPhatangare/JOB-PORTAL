3a792991744e23dc1b9166060ee4c1ff9fedeb42
Vedant <vedantphatangare10@gmail.com>
2025-03-15 01:47:07 +0530

    Lazy Loading added to Components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,52 +1,77 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Home from './pages/Home'
-import { Provider } from 'react-redux'
-import store, { persistor } from './app/Store'
-import JobDescription from './Components/JobDescription/JobDescription'
-import { PersistGate } from 'redux-persist/integration/react'
-import RecruiterHome from './pages/RecruiterHome'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import { lazy, Suspense } from "react";
 
-const Router  = createBrowserRouter([
-    {
-      path:"/",
-      element:<App/>,
-      children:[
-        {
-          path:'/',
-          element:<Home/>
-        },
-        {
-          path:"/recruiterhome",
-          element:<RecruiterHome/>
-        }
-        ,
-        {
-          path:"/login",
-          element:<Login/>
-        },
-        {
-          path:"/signup",
-          element:<Signup/>
-        },
-        {
-          path:"/job/:job_id",
-          // loader: Jobloader,
-          element:<JobDescription/>
-        }
-      ]
-    }
-])
+import Home from "./pages/Home";
 
+// import Login from './pages/Login'
+// import Signup from "./pages/Signup";
+// import RecruiterHome from "./pages/RecruiterHome";
+// import JobDescription from "./Components/JobDescription/JobDescription";
 
-createRoot(document.getElementById('root')!).render(
-  <Provider store = {store}>
+// lazy loading components
+const Login = lazy(() => import("./pages/Login"));
+const Signup = lazy(() => import("./pages/Signup"));
+const RecruiterHome = lazy(() => import("./pages/RecruiterHome"));
+const JobDescription = lazy(
+  () => import("./Components/JobDescription/JobDescription")
+);
+import { Provider } from "react-redux";
+import store, { persistor } from "./app/Store";
+import { PersistGate } from "redux-persist/integration/react";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/recruiterhome",
+        element: (
+          <Suspense fallback={<div>loading...</div>}>
+            <RecruiterHome />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/login",
+        element: (
+          <Suspense fallback={<div>loading...</div>}>
+            <Login />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <Suspense fallback={<div>loading...</div>}>
+            <Signup />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/job/:job_id",
+        // loader: Jobloader,
+        element: (
+          <Suspense fallback={<div>loading...</div>}>
+            <JobDescription />,
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-    <RouterProvider router={Router}/>
+      <RouterProvider router={Router} />
     </PersistGate>
   </Provider>
-)
+);
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,42 +1,58 @@
-import { useEffect, useState } from "react"
-import Jobcard from "../Components/JobCard/Jobcard"
-import { JobcardProps } from "../utils/types"
+import { lazy, Suspense, useEffect, useState } from "react";
+const Jobcard = lazy(() => import("../Components/JobCard/Jobcard"));
+// import Jobcard from "../Components/JobCard/Jobcard"
+import { JobcardProps } from "../utils/types";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setJobs } from "../features/Jobslice";
 import { Appdispatch } from "../app/Store";
 
-const Home:React.FC=()=>{   
-    const dispatch = useDispatch<Appdispatch>()
-    const [jobs, setjobs] = useState<JobcardProps[]>([]);
-    const getJobs = async()=>{
-        const response= await axios.get('http://127.0.0.1:5000/api/jobs/getjobs');
-        console.log(response);
-        setjobs(response.data.jobs);
-        dispatch(setJobs(response.data.jobs));
-    }
-    useEffect(() => {
-        getJobs();
-    }, [])
+const Home: React.FC = () => {
+  const dispatch = useDispatch<Appdispatch>();
+  const [jobs, setjobs] = useState<JobcardProps[]>([]);
+  const getJobs = async () => {
+    const response = await axios.get("http://127.0.0.1:5000/api/jobs/getjobs");
+    console.log(response);
+    setjobs(response.data.jobs);
+    dispatch(setJobs(response.data.jobs));
+  };
+  useEffect(() => {
+    getJobs();
+  }, []);
 
   return (
     <div className="relative w-full h-full flex justify-center items-center">
-        <div className="w-[70%] h-full flex pt-8">
-            <div className="w-[25%] h-full"></div>
-            <div className="jobs w-[50%] h-full">
-                {
-                    jobs?
-                    jobs.map((job:JobcardProps)=>
-                        <Jobcard key={job._id} {...job}/>
-                    )
-                    : "No Jobs"
-                }
-                
-            </div>
-            <div className="w-[25%] h-full"></div>
+      <div className="w-[70%] h-full flex pt-8">
+        <div className="w-[25%] h-full"></div>
+        <div className="jobs w-[50%] h-full">
+          {jobs
+            ? jobs.map((job: JobcardProps) => (
+                <Suspense
+                  fallback={
+                    <div className="w-full bg-gray-100 h-40 mb-2 mt-2 p-2 rounded-xl">
+                      <div className="bg-gray-200 w-50 h-4 rounded-sm"></div>
+                      <div className="bg-gray-200 w-20 h-4 rounded-sm mt-2"></div>
+                      <div className="ml-2 mt-5">
+                      <div className="bg-gray-200 w-12 h-4 rounded-sm mb-2"></div>
+                      <div className="bg-gray-200 w-12 h-4 rounded-sm"></div>
+                      </div>
+                      <div className="ml-2 mt-5 flex flex-row gap-4">
+                      <div className="bg-gray-200 w-28 h-4 rounded-sm"></div>
+                      <div className="bg-gray-200 w-20 h-4 rounded-sm"></div>
+                      </div>
+                    </div>
+                  }
+                >
+                  <Jobcard key={job._id} {...job} />
+                </Suspense>
+              ))
+            : "No Jobs"}
+             
         </div>
+        <div className="w-[25%] h-full"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
