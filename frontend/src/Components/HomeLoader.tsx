731f8c0e25a739303b76d5b20a9c0b54314223e2
Vedant <vedantphatangare10@gmail.com>
2025-03-17 02:50:38 +0530

    reAligned and added Loader

---
+++ frontend/src/Components/HomeLoader.tsx
@@ -0,0 +1,19 @@
+
+function HomeLoader() {
+  return (
+    <div className="w-full bg-gray-100 min-h-40 mb-2 mt-2 p-2 rounded-xl">
+      <div className="bg-gray-200 w-50 h-4 rounded-sm"></div>
+      <div className="bg-gray-200 w-20 h-4 rounded-sm mt-2"></div>
+      <div className="ml-2 mt-5">
+        <div className="bg-gray-200 w-12 h-4 rounded-sm mb-2"></div>
+        <div className="bg-gray-200 w-12 h-4 rounded-sm"></div>
+      </div>
+      <div className="ml-2 mt-5 flex flex-row gap-4">
+        <div className="bg-gray-200 w-28 h-4 rounded-sm"></div>
+        <div className="bg-gray-200 w-20 h-4 rounded-sm"></div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeLoader;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,13 +6,14 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setJobs } from "../features/Jobslice";
 import { Appdispatch } from "../app/Store";
+import HomeLoader from "../Components/HomeLoader";
 
 const Home: React.FC = () => {
   const dispatch = useDispatch<Appdispatch>();
   const [jobs, setjobs] = useState<JobcardProps[]>([]);
   const getJobs = async () => {
     const response = await axios.get("http://127.0.0.1:5000/api/jobs/getjobs");
-    console.log(response);
+    // console.log(response);
     setjobs(response.data.jobs);
     dispatch(setJobs(response.data.jobs));
   };
@@ -22,35 +23,21 @@ const Home: React.FC = () => {
 
   return (
     <div className="relative w-full h-full flex justify-center items-center">
-      <div className="w-[70%] h-full flex pt-8">
-        <div className="w-[25%] h-full"></div>
-        <div className="jobs w-[50%] h-full">
-          {jobs
-            ? jobs.map((job: JobcardProps) => (
-                <Suspense
-                  fallback={
-                    <div className="w-full bg-gray-100 h-40 mb-2 mt-2 p-2 rounded-xl">
-                      <div className="bg-gray-200 w-50 h-4 rounded-sm"></div>
-                      <div className="bg-gray-200 w-20 h-4 rounded-sm mt-2"></div>
-                      <div className="ml-2 mt-5">
-                      <div className="bg-gray-200 w-12 h-4 rounded-sm mb-2"></div>
-                      <div className="bg-gray-200 w-12 h-4 rounded-sm"></div>
-                      </div>
-                      <div className="ml-2 mt-5 flex flex-row gap-4">
-                      <div className="bg-gray-200 w-28 h-4 rounded-sm"></div>
-                      <div className="bg-gray-200 w-20 h-4 rounded-sm"></div>
-                      </div>
-                    </div>
-                  }
-                >
-                  <Jobcard key={job._id} {...job} />
-                </Suspense>
-              ))
-            : "No Jobs"}
-             
-        </div>
-        <div className="w-[25%] h-full"></div>
+      <div className="w-[30%] h-full "></div>
+      <div className="jobs w-[40%] h-full p-4 flex flex-row">
+        {jobs.length != 0 && jobs ? (
+          jobs.map((job: JobcardProps) => (
+            <Suspense fallback={<HomeLoader />}>
+              <Jobcard key={job._id} {...job} />
+            </Suspense>
+          ))
+        ) : (
+          <div className="bg-gray-200 h-30 w-full rounded-lg self-center flex justify-center items-center justify-self-center text-neutral-700 text-lg tracking-wide">
+            No Jobs Available
+          </div>
+        )}
       </div>
+      <div className="w-[30%] h-full "></div>
     </div>
   );
 };
