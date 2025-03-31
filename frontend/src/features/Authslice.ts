6e5b40dfe63f5f34b1c2e0bad0acd2b3f211c440
Vedant <vedantphatangare10@gmail.com>
2025-03-31 12:37:01 +0530

    added items i rootstate

--- frontend/src/features/Authslice.ts
+++ frontend/src/features/Authslice.ts
@@ -2,24 +2,29 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface AuthState {
     isAuthenticated: boolean;
-    role?:"Candidate" | "Recruiter" |""
+    role:"Candidate" | "Recruiter" |"",
+    id:string
 }
 
 const initialState: AuthState = {
     isAuthenticated: false,
-    role: ""
+    role: "",
+    id:""
 }
 
 export const AuthSlice= createSlice({
     name:"Auth",
     initialState,
     reducers:{
-        login(state, action:PayloadAction<"Candidate" | "Recruiter">){
+        login(state, action:PayloadAction<{role:"Candidate" | "Recruiter",id:string}>){
             state.isAuthenticated = true;
-            state.role = action.payload;
+            state.role = action.payload.role;
+            state.id = action.payload.id;
         },
         logout(state){
             state.isAuthenticated = false;
+            state.id= ""
+            state.role = ""
         }
     }
 })
