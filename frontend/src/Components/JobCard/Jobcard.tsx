d878331e5ddec1e3ceb2a3bd48bb16e8b7de185e
Vedant <vedantphatangare10@gmail.com>
2025-03-31 12:34:25 +0530

    jabCard css changes

--- frontend/src/Components/JobCard/Jobcard.tsx
+++ frontend/src/Components/JobCard/Jobcard.tsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router-dom";
 const Jobcard: React.FC<JobcardProps> = ({_id,title,company,location,salary,jobtype,postedBy}) => {
   const navigate = useNavigate()
   return (
-    <div className="flex flex-col justify-center items-center w-full h-[25%] rounded-xl bg-gray-50 p-2 mt-2 mb-2  cursor-pointer hover:bg-white transition-all hover:scale-[1.02] shadow-sm"
-    onClick={()=>navigate(`/job/:${_id}`)}>
+    <div className="flex flex-col justify-center items-center w-full h-[25%] rounded-lg bg-gray-50 p-2 mt-2 mb-2  cursor-pointer hover:bg-white hover:rounded-sm transition-all hover:scale-[1.03] duration-200 shadow-sm"
+    onClick={()=>navigate(`/job/${_id}`)}>
       
       <div className="flex flex-col mb-2 justify-center w-full">
         <div className="flex justify-between text-xl font-medium text-gray-900">
@@ -27,9 +27,9 @@ const Jobcard: React.FC<JobcardProps> = ({_id,title,company,location,salary,jobt
         </div>
       </div>
 
-      <div className="flex flex-row gap-4 w-full text-sm items-center">
-        <span className="bg-blue-100 text-blue-800 px-2 py-1 rounded-md">{jobtype}</span>
-        <div className="bg-green-100 text-green-800 px-2 py-1 rounded-md">posted by {postedBy.name}</div>
+      <div className="flex flex-row gap-4 w-full text-sm tracking-wide items-center">
+        <div className="bg-blue-100 text-blue-800 px-2 py-1 rounded-md flex items-center justify-center">{jobtype}</div>
+        <div className="bg-green-100 text-green-800 px-2 py-1 rounded-md flex items-center justify-center">posted by {postedBy.name}</div>
       </div>
 
     </div>
